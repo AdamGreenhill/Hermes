f6f35a8d3dab60d112287da36bd47722330524ee
Adam Greenhill <adam.greenhill@gmail.com>
2016-11-26 11:45:57 -0500

    MailGun confirmed working

--- hermes.py
+++ hermes.py
@@ -1,11 +1,40 @@
 import argparse
+import requests
 import smtplib
 import sys
 import time
 from email.mime.text import MIMEText
 
-def mailgun_emailer(api, domain, filename, subject, sender, recipient):
-        pass
+def mailgun_emailer(api, domain, filename, subject, sender, recipient, htmlbody, attachments):
+        url = 'https://api.mailgun.net/v3/{}/messages'.format(domain)
+        auth = ('api', api)
+        data = {}
+        files = {}
+
+        with open(filename) as fp:
+
+                if htmlbody:
+                        data = {
+                                'from':sender,
+                                'to':recipient,
+                                'subject':subject,
+                                'text': 'Please enable HTML to view contents of this e-mail.',
+                                'html': fp.read(),
+                        }
+                else:
+                        data = {
+                                'from':sender,
+                                'to':recipient,
+                                'subject':subject,
+                                'text': MIMEText(fp.read()),
+                        }
+
+                if attachments != "":
+                        response = requests.post(url, auth=auth, data=data, files=files)
+                        print("[!] Successfully sent API call to mailgun with attachment | Destination: %s", recipient)
+                else:
+                        response = requests.post(url, auth=auth, data=data)
+                        print("[!] Successfully sent API call to mailgun | Destination: %s", recipient)
 
 def gmail_emailer(username, password, filename, subject, sender, recipient):
         with open(filename) as fp:
@@ -25,9 +54,9 @@ def gmail_emailer(username, password, filename, subject, sender, recipient):
                                 '', body])
                 try:
                         s.sendmail(sender, recipient, msg)
-                        print("[!] Successfully sent g-mail")
+                        print("[!] Successfully sent gmail | Destination: %s", recipient)
                 except:
-                        print("[*] Error - could not send gmail")
+                        print("[*] Error - could not send gmail | Destination: %s", recipient)
 
 def postfix_emailer(filename, subject, sender, recipient):
         with open(filename) as fp:
@@ -41,19 +70,26 @@ def postfix_emailer(filename, subject, sender, recipient):
                 s = smtplib.SMTP('localhost')
                 try:
                         s.send_message(msg)
-                        print("[!] Successfully sent postfix email")
+                        print("[!] Successfully sent postfix email | Destination: %s", recipient)
                 except:
-                        print("[*] Error - could not send postfix email")
+                        print("[*] Error - could not send postfix email | Destination: %s", recipient)
                 s.quit()
 
 def email_controller(args, destination_email):
         # Decide where to route e-mails to
         if args.infrastructure == "mailgun":
-                pass
+                mailgun_emailer(args.api, args.domain, args.body, args.subject, args.email_author, destination_email, args.enable_html, args.attachment)
         elif args.infrastructure == "gmail":
                 gmail_emailer(args.username, args.password, args.body, args.subject, args.email_author, destination_email)
         elif args.infrastructure == "postfix":
                 postfix_emailer(args.body, args.subject, args.email_author, destination_email)
+
+def banner():
+        print("'||'  '||' '||''''|  '||''|.   '||    ||' '||''''|   .|'''.| ")
+        print(" ||    ||   ||  .     ||   ||   |||  |||   ||  .     ||..  ' ")
+        print(" ||''''||   ||''|     ||''|'    |'|..'||   ||''|      ''|||. ")
+        print(" ||    ||   ||        ||   |.   | '|' ||   ||       .     '||")
+        print(".||.  .||. .||.....| .||.  '|' .|. | .||. .||.....| |'....|' ")
 
 def main():
         # Create initial definition of variables
@@ -76,9 +112,12 @@ def main():
         gmail.add_argument('-u','--username', help='Username to authenticate with')
         gmail.add_argument('-p','--password', help='Password to authenticate with')
         mailgun.add_argument('-a','--api', help="MailGun API key to authenticate with")
+        mailgun.add_argument('--domain', help="Domain to send MailGun e-mail from")
         recipients.add_argument('-r','--recipient', help='Singular destination to send e-mail to.')
         recipients.add_argument('-l','--list-of-emails', help='Textfile containing a list of destination e-mails')
         optional.add_argument('-d', '--delay', help="Delay the sending of e-mails by a number of seconds.", type=int)
+        optional.add_argument('--attachment', help="Add an attachment to the e-mail you're sending")
+        optional.add_argument('--enable-html', help="Enable rich text e-mails. This will send rendered HTML in the e-mail.")
 
         args = parser.parse_args()
 
@@ -89,15 +128,17 @@ def main():
                         if args.list_of_emails:
                                 print("[*] Error - passing wrong arguments. -r/--recipient and -l/--list-of-emails are mutually exclusive.")
                                 sys.exit(1)
+                        banner()
                         email_controller(args, args.recipient)
                 elif args.list_of_emails:
                         with open(args.list_of_emails) as fp:
                                 for line in fp:
+                                        banner()
                                         email_controller(args, line)
                                 if args.d:
                                         time.sleep(args.d)
                 else:
-                        print("[*] Error - passing insufficient arguments.")
+                        print("[*] Error - need to pass recipient(s). This can be done singular (-r) or through a textfile list (-l).")
         else:
-                parser.print_help()      
+                parser.print_help()
 main()
